817feacf5648d11d3251cd5558b5efcbabcb04d8
Jon Nichols <jnichols@steamstreet.com>
2022-12-11 23:39:44 -0500

    Remove environment requirements (defaults appear to be fine)

--- .space.kts
+++ .space.kts
@@ -1,10 +1,5 @@
 job("compile") {
     container("amazoncorretto:17-alpine") {
-        resources {
-            cpu = 500.mcpu
-            memory = 2000.mb
-        }
-
         kotlinScript {
             it.gradlew("publish", "-PBUILD_NUMBER=${it.executionNumber()}")
         }
