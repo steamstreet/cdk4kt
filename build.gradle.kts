65c1457f3101d1dc5e30e959e264e6e5feefb123
Jon Nichols <jnichols@steamstreet.com>
2023-03-31 10:32:50 -0700

    Print the release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ val cdkVersion = findProperty("cdkVersion") as String
 val kotlinVersion = findProperty("kotlinVersion") as String
 val releaseVersion = findProperty("releaseVersion") as? String
 
+println("Release version: ${releaseVersion}")
+
 allprojects {
     group = "com.steamstreet"
     version = releaseVersion ?: "$cdkVersion-SNAPSHOT"
